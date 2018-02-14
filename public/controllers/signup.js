eb0f47bf46e83919f46b33b78f0551bb740daa01
ashish1508 <belide.ashish@gmail.com>
2018-02-14 22:49:49 +0530

    Update signup.js

--- public/controllers/signup.js
+++ public/controllers/signup.js
@@ -1,10 +1,11 @@
 var signup = angular.module('signup',[]);
-signup.controller('signupcontrol',['$scope','$http',function($scope,$http){
+signup.controller('signupcontrol',['$scope','$http','$window',function($scope,$http,$window){
 	$scope.use ={};
 	
 	$scope.signup = function(){
 		$http.post('/signup',$scope.use).when(function(res){
 			console.log("success");
+			$window.location.href = 'https://gentle-plains-65118/login';
 		})
 	}
 }])
